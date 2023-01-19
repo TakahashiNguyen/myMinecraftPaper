e6b5f4010e5b7c837c17ecb244e62896cb6a03a9
nVietUK <vietuk.nguyen.git@icloud.com>
2023-01-19 14:43:23 +0700

    update

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,2 @@
-pkg install apt curl wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
-chmod +x start-ubuntu.sh
-./start-ubuntu.sh
-apt install software-properties-common -y
-add-apt-repository ppa:openjdk-r/ppa -y && apt update
 apt install openjdk-17 -ya
 wget -O server.jar https://api.papermc.io/v2/projects/paper/versions/1.19.3/builds/380/downloads/paper-1.19.3-380.jar
